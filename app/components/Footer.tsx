116442bfd080d03a546e4cc4861618850e3b6db1
NeiJunio <neijunio50@gmail.com>
2024-12-05 23:44:13 -0300

    add Header and Footer components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="py-6 mb-8">
+                <p className="text-center">Made by Nei Junio</p>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
+import {Footer, Header} from "./components"
+
 export default function Home() {
-  return (
-    <>
-    <header>This is the header</header>
-    <main className="flex-1">This is the main</main>
-    <footer>This is the footer</footer>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main className="flex-1">This is the main</main>
+            <Footer />
+        </>
+    );
 }
